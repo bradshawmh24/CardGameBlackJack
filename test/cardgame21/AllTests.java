da35d8f1b7a9dcecfa6217f097cce9bbffb05aa8
bradshawmh24 <mbradshaw@utah.gov>
2017-01-12 14:12:59 -0700

    Fixed code

--- test/cardgame21/AllTests.java
+++ test/cardgame21/AllTests.java
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-/**
- *
- * @author Andrew Maynez, Michael Bradshaw, Hoa Silim
+/** @author Andrew Maynez, Michael Bradshaw, Hoa Silim
  */
 
 package cardgame21;
@@ -15,21 +12,3 @@ import org.junit.runners.Suite.SuiteClasses;
 public class AllTests {
 
 }
-=======
-/**
- *
- * @author Andrew Maynez, Michael Bradshaw, Hoa Silim
- */
-
-package cardgame21;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
-
-@RunWith(Suite.class)
-@SuiteClasses({ BettingTest.class, CardGame21Test.class, CardValuesTest.class, HandTest.class })
-public class AllTests {
-
-}
->>>>>>> origin/master
--- test/cardgame21/HandTest.java
+++ test/cardgame21/HandTest.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /**
  *
  * @author Andrew Maynez, Michael Bradshaw, Hoa Silim
@@ -22,28 +22,3 @@ public class HandTest {
 	}
 
 }
-=======
-/**
- *
- * @author Andrew Maynez, Michael Bradshaw, Hoa Silim
- */
-
-package cardgame21;
-
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
-public class HandTest {
-
-	@Test
-	public void testCardNums() {
-		String deuceCard = "DEUCE";
-		assertTrue("String 'DEUCE' sets card value = 2", (deuceCard == "DEUCE") );
-		
-		String aceCard = "ACE";
-		assertTrue("String 'ACE' sets card value = 11", (aceCard == "ACE") );
-	}
-
-}
->>>>>>> origin/master
